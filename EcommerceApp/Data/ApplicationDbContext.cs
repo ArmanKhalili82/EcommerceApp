ce6b25f3f4bc7f9309bdd38858e6f6ef2242761a
ArmanKhalili82 <hykhalili@yahoo.com>
2024-06-10 22:08:21 +0200

    Add project files.

---
+++ EcommerceApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EcommerceApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+    }
+}
